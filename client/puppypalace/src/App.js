9c7f32ce47c89d3c5507d0a10e55a6dd15b32faf
Daniel Londoño <danielondonoa@gmail.com>
2022-03-02 17:12:37 -0500

    Forms 1 (#15)
    Co-authored-by: OneTwoDaniel <66371572+OneTwoDaniel@users.noreply.github.com>

--- client/puppypalace/src/App.js
+++ client/puppypalace/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home/Home';
 import React from 'react';
 import './App.css';
+import LogIn from './components/Forms/LogIn';
+import Register from './components/Forms/Register'
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/formlogin' element={<LogIn />}/>
+        <Route path='/formnewuser' element={<Register />}/>
       </Routes>
     </div>
     </BrowserRouter>
