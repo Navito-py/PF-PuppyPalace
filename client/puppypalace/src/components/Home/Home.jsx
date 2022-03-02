1b0c9d25e23eee0c3a97b06ab549948a040561ad
OneTwoDaniel <66371572+OneTwoDaniel@users.noreply.github.com>
2022-03-02 17:09:29 -0500

    Forms 1

--- client/puppypalace/src/components/Home/Home.jsx
+++ client/puppypalace/src/components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import { Link } from "react-router-dom";
 
-export default function Home(){
-    return(
-        <div>
-            <NavBar />
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <Link to="/formnewuser">
+        <button>Crear usuario</button>
+      </Link>
+      <Link to="/formlogin">
+        <button>Login</button>
+      </Link>
+    </div>
+  );
 }
