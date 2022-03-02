b9920af318a168c21e132597c29bb2c630d072e6
Hernan Cabrera Z <hcabrerazavala@gmail.com>
2022-03-02 17:00:30 -0300

    Deployablelist (#12)
    * redux elements
    
    * navbar y home

--- client/puppypalace/src/App.js
+++ client/puppypalace/src/App.js
@@ -1,9 +1,17 @@
+
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home/Home';
 import React from 'react';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
