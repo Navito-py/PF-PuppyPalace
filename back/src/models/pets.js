971f968cb60e6ef142c155b9674e3db226d785ee
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-03-02 14:24:14 -0300

    Pets models created

---
+++ back/src/models/pets.js
@@ -0,0 +1,57 @@
+const { DataTypes } = require ('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('pets', {
+        
+        id: {
+            type: DataTypes.UUID,
+            defaulValue: DataTypes.UUIDV4,
+            unique: true,
+            allowNull: false,
+            primaryKey: true
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true
+            }
+        },
+
+        type: { // Perro o Gato, no puede ser otra cosa
+            type: DataTypes.ENUM("Dog", "Cat"),
+            allowNull: false
+        },
+
+        breed: { // Raza
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+
+        height: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+
+        weight: {
+            type: DataTypes.FLOAT,
+            allowNull: true
+        },
+
+        image: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            defaultValue: "https://www.segurmaniazurekin.eus/a/2020/11/segurmania_mascotas_destacada-320x240.jpg",
+            validate: {
+                isUrl: true
+            }
+        },
+
+    }, {timestamps: false})
+}
