5360c971e25d80d1250446c32f0407e627287e91
ivan ariel toneatto <ivan-ariel@hotmail.com>
2022-03-03 12:21:27 -0300

    creacion de componente detail

--- client/puppypalace/src/components/Card/Card.jsx
+++ client/puppypalace/src/components/Card/Card.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Card({name, image, schedule}) {
     return (
         <div>
-            <h3>{name}</h3>
+            <h4>{name}</h4>
             <img src={image} alt='' width='230px' height='160px' />
-            <h3>{schedule}</h3>
+            <p>{schedule}</p>
         </div>
     )
 }
