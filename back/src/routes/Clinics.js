9e51b16ba21f665bf9f169c0b3887a7b28e00b8b
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-03 18:54:54 -0300

    arreglos en models & routes

--- back/src/routes/Clinics.js
+++ back/src/routes/Clinics.js
@@ -1,9 +1,10 @@
 const {Router} = require ('express');
-
+const {getClinics} = require("../controllers/Clinics.js");
+const {postClinics} = require("../controllers/Clinics.js");
 
 const router = Router();
 
-router.get('/',);
-router.post('/', );//acá va la ruta)
+router.get('/', getClinics);
+router.post('/', postClinics );
 
 module.exports = router;
