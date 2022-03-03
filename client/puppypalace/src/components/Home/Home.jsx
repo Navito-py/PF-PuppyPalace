46dd4e7d036a8b2c3ffa2264cd236cda9d430130
marianofarrate <igorandrev@hotmail.com>
2022-03-03 12:43:30 -0300

    searchbar y ruta 404

--- client/puppypalace/src/components/Home/Home.jsx
+++ client/puppypalace/src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <SearchBar/>
       <Link to="/formnewuser">
         <button>Crear usuario</button>
       </Link>
