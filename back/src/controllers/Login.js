3b6190a529ed4e24e9e0b30db4e87a7216c08deb
lautaro321 <skoollarris9@gmail.com>
2022-03-04 13:40:26 -0300

    ruta login

--- back/src/controllers/Login.js
+++ back/src/controllers/Login.js
@@ -1,9 +1,37 @@
 //Importaciones
-
+const {User, Pet, Vaccine}= require ('../db.js');
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // FUNCIONES
 
+const login = async (req , res , next) =>{
+    try{
+        const {
+
+            userName,
+            password,
+             
+        }
+        =req.body;
+        
+        const user = await User.findOne({
+            where:{
+                userName,
+                
+            }
+        });
+        if(!user) return res.status(404).send({error: "usuario no encontrado"});
+
+        if(user.password !== password) return res.status(401).send({error: "contraseña incorrecta"});
+
+        if(user.password === password) return res.status(200).send(user);
+
+    }catch (error){
+    next(error)
+    }
+
+}
+module.exports = {login}
 
 
 
