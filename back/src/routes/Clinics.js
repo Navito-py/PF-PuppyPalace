728c8823b3454204b64f0aa71ae668efbdb369c8
Luciano Staniszewski <91505393+LucjanStaniszew@users.noreply.github.com>
2022-03-04 17:35:59 -0300

    Search routes clinics (#29)
    * Clinics Route controller for query & id Search
    
    * Added id route

--- back/src/routes/Clinics.js
+++ back/src/routes/Clinics.js
@@ -1,10 +1,12 @@
 const {Router} = require ('express');
 const {getClinics} = require("../controllers/Clinics.js");
 const {postClinics} = require("../controllers/Clinics.js");
+const {getClinicsId} = require("../controllers/Clinics.js");
 
 const router = Router();
 
 router.get('/', getClinics);
+router.get('/:id', getClinicsId);
 router.post('/', postClinics );
 
 module.exports = router;
