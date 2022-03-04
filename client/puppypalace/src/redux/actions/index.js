8b8bff09400d8a46006c74432ab8a9c4263b7486
Daniel Londoño <danielondonoa@gmail.com>
2022-03-04 11:09:31 -0500

    actions/reducers nuevos (#24)
    Co-authored-by: OneTwoDaniel <66371572+OneTwoDaniel@users.noreply.github.com>

--- client/puppypalace/src/redux/actions/index.js
+++ client/puppypalace/src/redux/actions/index.js
@@ -1,5 +1,33 @@
-//import axios from 'axios';
+import axios from 'axios';
 
 export function getNameClinic(name) {
  
+}
+
+export function getClinics(){
+    return async function(dispatch) {
+        try {
+            const responseClinics = await axios.get("http://localhost:3001/clinics")
+            return dispatch({
+                type: "GET_CLINICS",
+                payload: responseClinics.data
+            })
+        } catch {
+            return alert("No se encontraron veterinarias")
+        }
+    }
+}
+
+export function getDetail(id){
+    return async function (dispatch){
+        try{
+            const responseDetail = await axios.get(`http://localhost:3001/clinics/${id}`)
+            return dispatch({
+                type: "GET_DETAILS",
+                payload: responseDetail.data
+            })
+        } catch(error){
+            console.log(error)
+        }
+    }
 }
--- client/puppypalace/src/redux/reducer/index.js
+++ client/puppypalace/src/redux/reducer/index.js
@@ -1,9 +1,23 @@
 const initialState = {
+  clinics: [],
+  detail: [],
+};
 
-}
-
-function rootReducer(state = initialState, action){
-
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_CLINICS":
+      return {
+        ...state,
+        clinics: action.payload,
+      };
+    case "GET_DETAILS":
+      return {
+        ...state,
+        detail: action.payload,
+      };
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
