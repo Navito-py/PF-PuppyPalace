e353561c8f875fca5ec86ccd1b935c6af9fef299
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-04 17:07:15 -0300

    cambios en la base de datos

--- back/index.js
+++ back/index.js
@@ -1,9 +1,24 @@
 const server = require('./src/app.js');
+const {Clinic} = require("./src/db.js");
 const { conn } = require('./src/db.js');
+const {clinicas} = require("./objectsClinics.js");
+
+
+const eraseDataBase = true;
+
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: eraseDataBase }).then(() => {
+
+  if(eraseDataBase){
+    createClinics();
+  }
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
+
+
+const createClinics = async () =>{
+  await Clinic.bulkCreate(clinicas);
+}
