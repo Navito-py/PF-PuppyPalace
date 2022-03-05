eac7066692b4ab552eb3fe59d9399d9e545f9ec1
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-05 20:07:20 -0300

    y ahora si esta listo (espero)

--- back/objectsClinics.js
+++ back/objectsClinics.js
@@ -1,149 +1,151 @@
 const clinicas = [
-    {
-    "name" : "Clinica Veterinaria Dr. Munini",
-    "address" : "Paraguay 2773, Rosario, Santa Fe", 
-    "province": "Santa Fé" ,
-    "city": "Rosario",
-    "activeHours": "Lunes a Viernes 8 hs a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3414824562,
-    "image" : "https://lirp.cdn-website.com/543a3f93/dms3rep/multi/opt/2272218-img_1-45c48-1920w.jpg"
-     },
-   
-   {
-    "name" : "Mascotas del Oeste",
-    "address" : "Bv. Avellaneda 1153",
-    "province": "Santa Fé" ,
-    "city": "Rosario",
-    "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 16 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3414226932,
-    "image" : "https://lh5.googleusercontent.com/p/AF1QipM1JyT0JLKdwj3pAd0EDj04s6jgg6hGePzdSOkJ=w750-h606-p-k-no"
-    },
-   {
-    "name" : "Veterinaria San Bernardo",
-    "address" : "Bv. Seguí 3506", 
-    "province": "Santa Fé" ,
-    "city": "Rosario",
-    "activeHours": "Lunes a Viernes 9:30 a 13 hs // 17 a 19 hs // Sábado 9 a 13 hs",
-    "phone" : 3412419480,
-    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/97090844_107974600920563_1637944902849396736_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=973b4a&_nc_ohc=JE9TuBPo-TgAX8OWzof&_nc_ht=scontent.fcor10-3.fna&oh=00_AT9xoI2z4otcKek1g0nfVgCC6lUMXn4PP2LNtIdaf7ZdrQ&oe=624A303D"
-    },
-   {
-    "name" : "Asistencia Veterinaria",
-    "address" : "Av. Sarmiento 905", 
-    "province": "Santa Fé" ,
-    "city": "Rosario",
-    "activeHours": "Lunes a Viernes 9 a 12 hs // 17 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3413076530
-    },
-   
-   {
-    "name" : "Mascota 24",
-    "address" : "Dr. Estanislao S. Zeballos 1425", 
-    "province": "Santa Fé" ,
-    "city": "Rosario",
-    "activeHours": "24 hs",
-    "phone" : 3414204880,
-    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/138847523_2832706463662131_3931400919551961485_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9mcKE7NUWtAAX--kflr&_nc_ht=scontent.fcor10-3.fna&oh=00_AT98Yy1ohfZtqo9xYfvpEV-STiluX_Ipwu2zv9BO4jsi0g&oe=6248A291"
-    },
-   
-   {
-    "name" : "Clínica Veterinaria Francesa",
-    "address" : "Buenos Aires 373", 
-    "province": "Mendoza",
-    "city": "Mendoza",
-    "activeHours": "Lunes a Viernes 9 a 20 hs // Sábado 9 a 13 hs",
-    "phone" :  2614234011
-    },
-   
-   {
-    "name" : "Vet House Clínica Veterinaria",
-    "address" : "Juan Gualberto Godoy 181", 
-    "province": "Mendoza" ,
-    "city": "Mendoza",
-    "activeHours": "Lunes a Viernes 9 a 19 hs // Sábado 9 a 13 hs",
-    "phone" : 2615405474,
-    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/122866231_2758624857711651_5358198321668575421_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=973b4a&_nc_ohc=a8ZXYgJCT4UAX_UKSVU&_nc_ht=scontent.fcor10-3.fna&oh=00_AT_QiC8YBzLQQTkmuqxH29GuMlaciHFuFhCIoGL2AE_E9g&oe=6249B085"
-    },
-   
-   {
-    "name" : "Veterinaria Mendoza Unimev",
-    "address" : "Bernardo Houssay 612", 
-    "province": "Mendoza" ,
-    "city": "Mendoza",
-    "activeHours": "Lunes a Sábado 9:30 a 13:30 hs // 17 a 21 hs",
-    "phone" : 02614215216,
-    "image": "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/121625061_3151775641612508_8632100231021941894_n.jpg?stp=cp0_dst-jpg_e15_p403x403_q65&_nc_cat=110&ccb=1-5&_nc_sid=9e2e56&_nc_ohc=mJM-FnJdmrkAX9CyJwB&_nc_ht=scontent.fcor10-3.fna&oh=00_AT9g4GwfMa_X54Ef5xs3yG3y_qxb-9_xn9aBdYyn7nL4Tw&oe=6248E0E4"
-    },
-   
-   {
-    "name" : "Veterinaria San Roque, Hospital Veterinaria",
-    "address" : "Av. Juan B. Justo 535", 
-    "province": "Mendoza" ,
-    "city": "Mendoza",
-    "activeHours": "Lunes a Viernes 9:30 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 2614233937,
-    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t31.18172-8/28828308_1973499366299782_3604011053253240126_o.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=FLGx_nE1Z1kAX_muP_b&_nc_ht=scontent.fcor10-3.fna&oh=00_AT_1FrLA1uJBBUwXQAaLfrMrWGNQ6-g-tenpyx_1A_RWHg&oe=62485B03"
-    },
-   
-   {
-    "name" : "Veterinaria Dr. de la Torre",
-    "address" : "Jorge A. Calle 224", 
-    "province": "Mendoza" ,
-    "city": "Mendoza",
-    "activeHours": "24 hs",
-    "phone" : 2614297168,
-    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.18169-9/312384_211083065701395_954552857_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=de6eea&_nc_ohc=esfAqBTCU8sAX9a8_D1&_nc_ht=scontent.fcor10-3.fna&oh=00_AT-ETHLC-cfk9NYQkx78oeJyswQSEH5LLYGf0l5epBPQCw&oe=6246639F"
-    },
-   
-   {
-    "name" : "Clínica Veterinaria Eva Inguerman",
-    "address" : "Av. Colón 6200", 
-    "province": "Córdoba",
-    "city": "Córdoba",
-    "activeHours": "24 hs",
-    "phone" : 3514326414,
-    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/46505973_328595631057966_4511639080299134976_n.png?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=d8ZMD_WaEHoAX8FsShD&_nc_ht=scontent.fcor10-3.fna&oh=00_AT81xlnItWrnrnC7vvMk5-7JdLA-DoNSsHrvDwEnZV5tvw&oe=624788BD"
-    },
-   
-   {
-    "name" : "Clínica Veterinaria Colón ",
-    "address" : "Duarte Quirós 5275", 
-    "province": "Córdoba",
-    "city": "Córdoba",
-    "activeHours": "Lunes a Viernes de 9:30 a 12:30 hs // 17 a 20 hs// Sábado 9 a 13 hs",
-    "phone" : 3514842227,
-    "image" : "https://entornosaludable.com/wp-content/uploads/2019/09/papelmatic-higiene-profesional-limpieza-desinfeccion-clinicas-veterinarias-980x617.jpg"
-    },
-   
-   {
-    "name" : "Clínica Veterinaria Córdoba Ser-Vet",
-    "address" : "Av. Dr Arturo Capdevilla 2087", 
-    "province": "Córdoba",
-    "city": "Córdoba",
-    "activeHours": "Lunes a Viernes de 9:30 js // 17 a 20 hs hs // // Sábado 9 a 13 hs",
-    "phone" : 3514791679,
-     "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/81845564_142874087151957_6660466584911347712_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=973b4a&_nc_ohc=Vi_w46p9gOgAX_PJq4r&_nc_ht=scontent.fcor10-3.fna&oh=00_AT_1dMQX7eJQIVOuwYJHsMc_ZmjUUR9pNjJZGeg2AQ_Zag&oe=624680C3"
-    },
-   
-   {
-    "name" : "Clínica Veterinaria Albeytor",
-    "address" : "Av Patria 304", 
-    "province": "Córdoba",
-    "city": "Córdoba",
-    "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 17 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3517631416,
-    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/121257614_1668201766676249_1916816802111887691_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=973b4a&_nc_ohc=DtYnf6JJAAIAX_jkTQz&_nc_ht=scontent.fcor10-3.fna&oh=00_AT_RICEhHswR_qOCDQaTX6gR78mTx_DLy5otxSCe3h38fQ&oe=624762E1"
+  {
+  "name" : "Clinica Veterinaria Dr. Munini",
+  "address" : "Paraguay 2773, Rosario, Santa Fe", 
+  "province": "Santa Fé" ,
+  "city": "Rosario",
+  "activeHours": "Lunes a Viernes 8 hs a 20 hs // Sábado 9 a 13 hs",
+  "phone" : 3414824562,
+  "image" : "https://images.pexels.com/photos/5731866/pexels-photo-5731866.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
    },
-   
-   {
-    "name" : "Bicho's Clínica Veterinaria",
-    "address" : "Av. Fuerza Aérea Argentina 2949", 
-    "province": "Córdoba",
-    "city": "Córdoba",
-    "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 17 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3514658784,
-     "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/81963394_1269534669913717_5193143501323239424_n.png?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=l7j1BHheP8UAX8Qk0oG&_nc_ht=scontent.fcor10-3.fna&oh=00_AT8ODzj_IQsTyNEeS6N3yZERoHP4VzdMQjj74ZQRLag35A&oe=62491ACE"
-    }
-   ];
-
+ 
+ {
+  "name" : "Mascotas del Oeste",
+  "address" : "Bv. Avellaneda 1153",
+  "province": "Santa Fé" ,
+  "city": "Rosario",
+  "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 16 a 20 hs // Sábado 9 a 13 hs",
+  "phone" : 3414226932,
+  "image" : "https://images.pexels.com/photos/6235647/pexels-photo-6235647.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ {
+  "name" : "Veterinaria San Bernardo",
+  "address" : "Bv. Seguí 3506", 
+  "province": "Santa Fé" ,
+  "city": "Rosario",
+  "activeHours": "Lunes a Viernes 9:30 a 13 hs // 17 a 19 hs // Sábado 9 a 13 hs",
+  "phone" : 3412419480,
+  "image" : "https://images.pexels.com/photos/5733422/pexels-photo-5733422.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ {
+  "name" : "Asistencia Veterinaria",
+  "address" : "Av. Sarmiento 905", 
+  "province": "Santa Fé" ,
+  "city": "Rosario",
+  "activeHours": "Lunes a Viernes 9 a 12 hs // 17 a 20 hs // Sábado 9 a 13 hs",
+  "phone" : 3413076530,
+  "image" : "https://images.pexels.com/photos/7474560/pexels-photo-7474560.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ 
+ {
+  "name" : "Mascota 24",
+  "address" : "Dr. Estanislao S. Zeballos 1425", 
+  "province": "Santa Fé" ,
+  "city": "Rosario",
+  "activeHours": "24 hs",
+  "phone" : 3414204880,
+  "image" : "https://images.pexels.com/photos/6234980/pexels-photo-6234980.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ 
+ {
+  "name" : "Clínica Veterinaria Francesa",
+  "address" : "Buenos Aires 373", 
+  "province": "Mendoza",
+  "city": "Mendoza",
+  "activeHours": "Lunes a Viernes 9 a 20 hs // Sábado 9 a 13 hs",
+  "phone" :  2614234011,
+  "image" : "https://media.istockphoto.com/photos/young-happy-veterinary-nurse-smiling-while-playing-with-a-dog-high-picture-id1303362255?b=1&k=20&m=1303362255&s=170667a&w=0&h=ebm7UoUmaCTxepZ9NoyPRTK2WC-5TmTlk6PyXou_NFg="
+  },
+ 
+ {
+  "name" : "Vet House Clínica Veterinaria",
+  "address" : "Juan Gualberto Godoy 181", 
+  "province": "Mendoza" ,
+  "city": "Mendoza",
+  "activeHours": "Lunes a Viernes 9 a 19 hs // Sábado 9 a 13 hs",
+  "phone" : 2615405474,
+  "image" : "https://media.istockphoto.com/photos/image-of-male-doctor-veterinarian-with-stethoscope-is-holding-cute-picture-id1294792706?b=1&k=20&m=1294792706&s=170667a&w=0&h=hq8H-P-omwVecfNPnuECsfXTo33rfKajIK1RRJfP0xs="
+  },
+ 
+ {
+  "name" : "Veterinaria Mendoza Unimev",
+  "address" : "Bernardo Houssay 612", 
+  "province": "Mendoza" ,
+  "city": "Mendoza",
+  "activeHours": "Lunes a Sábado 9:30 a 13:30 hs // 17 a 21 hs",
+  "phone" : 02614215216,
+  "image": "https://media.istockphoto.com/photos/vet-doctor-examining-labrador-dog-picture-id1303833951?b=1&k=20&m=1303833951&s=170667a&w=0&h=kZOqKof2-6XnBpkFzRqcJTitSu9_4uxmcYXiJp4aXMk="
+  },
+ 
+ {
+  "name" : "Veterinaria San Roque, Hospital Veterinaria",
+  "address" : "Av. Juan B. Justo 535", 
+  "province": "Mendoza" ,
+  "city": "Mendoza",
+  "activeHours": "Lunes a Viernes 9:30 a 20 hs // Sábado 9 a 13 hs",
+  "phone" : 2614233937,
+  "image" : "https://images.pexels.com/photos/4917820/pexels-photo-4917820.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ 
+ {
+  "name" : "Veterinaria Dr. de la Torre",
+  "address" : "Jorge A. Calle 224", 
+  "province": "Mendoza" ,
+  "city": "Mendoza",
+  "activeHours": "24 hs",
+  "phone" : 2614297168,
+  "image" : "https://images.pexels.com/photos/4580783/pexels-photo-4580783.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ 
+ {
+  "name" : "Clínica Veterinaria Eva Inguerman",
+  "address" : "Av. Colón 6200", 
+  "province": "Córdoba",
+  "city": "Córdoba",
+  "activeHours": "24 hs",
+  "phone" : 3514326414,
+  "image" : "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ 
+ {
+  "name" : "Clínica Veterinaria Colón ",
+  "address" : "Duarte Quirós 5275", 
+  "province": "Córdoba",
+  "city": "Córdoba",
+  "activeHours": "Lunes a Viernes de 9:30 a 12:30 hs // 17 a 20 hs// Sábado 9 a 13 hs",
+  "phone" : 3514842227,
+  "image" : "https://images.pexels.com/photos/320014/pexels-photo-320014.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ 
+ {
+  "name" : "Clínica Veterinaria Córdoba Ser-Vet",
+  "address" : "Av. Dr Arturo Capdevilla 2087", 
+  "province": "Córdoba",
+  "city": "Córdoba",
+  "activeHours": "Lunes a Viernes de 9:30 js // 17 a 20 hs hs // // Sábado 9 a 13 hs",
+  "phone" : 3514791679,
+   "image" : "https://images.pexels.com/photos/1741205/pexels-photo-1741205.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+ 
+ {
+  "name" : "Clínica Veterinaria Albeytor",
+  "address" : "Av Patria 304", 
+  "province": "Córdoba",
+  "city": "Córdoba",
+  "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 17 a 20 hs // Sábado 9 a 13 hs",
+  "phone" : 3517631416,
+  "image" : "https://images.pexels.com/photos/825947/pexels-photo-825947.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+ },
+ 
+ {
+  "name" : "Bicho's Clínica Veterinaria",
+  "address" : "Av. Fuerza Aérea Argentina 2949", 
+  "province": "Córdoba",
+  "city": "Córdoba",
+  "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 17 a 20 hs // Sábado 9 a 13 hs",
+  "phone" : 3514658784,
+   "image" : "https://images.pexels.com/photos/1612861/pexels-photo-1612861.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  }
+ ];
+ 
    module.exports = {clinicas};
