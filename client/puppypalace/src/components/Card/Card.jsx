645ef3620478ab6a3419e1dc1299ac3b2e71439a
ivan ariel toneatto <ivan-ariel@hotmail.com>
2022-03-05 20:34:05 -0300

    arreglo de cards

--- client/puppypalace/src/components/Card/Card.jsx
+++ client/puppypalace/src/components/Card/Card.jsx
@@ -12,7 +12,7 @@ export default function Card({ name, image, schedule }) {
   return (
     <div>
       <MDBCard style={{ maxWidth: "22rem" }}>
-        <MDBCardImage src={image} position="top" alt="..." />
+        <MDBCardImage src={image} position="top" alt="..." height='200px' whidth='' />
         <MDBCardBody>
           <MDBCardTitle>{name}</MDBCardTitle>
           <MDBCardText>{schedule}</MDBCardText>
