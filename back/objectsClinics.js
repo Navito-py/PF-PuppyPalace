2e41fff58ac9aba3df5ece9774b05b17fe32a3cf
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-05 17:45:57 -0300

    txt actualizado

--- back/objectsClinics.js
+++ back/objectsClinics.js
@@ -5,8 +5,8 @@ const clinicas = [
     "province": "Santa Fé" ,
     "city": "Rosario",
     "activeHours": "Lunes a Viernes 8 hs a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3414824562
-   
+    "phone" : 3414824562,
+    "image" : "https://lirp.cdn-website.com/543a3f93/dms3rep/multi/opt/2272218-img_1-45c48-1920w.jpg"
      },
    
    {
@@ -15,9 +15,8 @@ const clinicas = [
     "province": "Santa Fé" ,
     "city": "Rosario",
     "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 16 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3414226932
-   
-    
+    "phone" : 3414226932,
+    "image" : "https://lh5.googleusercontent.com/p/AF1QipM1JyT0JLKdwj3pAd0EDj04s6jgg6hGePzdSOkJ=w750-h606-p-k-no"
     },
    {
     "name" : "Veterinaria San Bernardo",
@@ -25,8 +24,8 @@ const clinicas = [
     "province": "Santa Fé" ,
     "city": "Rosario",
     "activeHours": "Lunes a Viernes 9:30 a 13 hs // 17 a 19 hs // Sábado 9 a 13 hs",
-    "phone" : 3412419480
-   
+    "phone" : 3412419480,
+    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/97090844_107974600920563_1637944902849396736_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=973b4a&_nc_ohc=JE9TuBPo-TgAX8OWzof&_nc_ht=scontent.fcor10-3.fna&oh=00_AT9xoI2z4otcKek1g0nfVgCC6lUMXn4PP2LNtIdaf7ZdrQ&oe=624A303D"
     },
    {
     "name" : "Asistencia Veterinaria",
@@ -35,7 +34,6 @@ const clinicas = [
     "city": "Rosario",
     "activeHours": "Lunes a Viernes 9 a 12 hs // 17 a 20 hs // Sábado 9 a 13 hs",
     "phone" : 3413076530
-   
     },
    
    {
@@ -44,9 +42,8 @@ const clinicas = [
     "province": "Santa Fé" ,
     "city": "Rosario",
     "activeHours": "24 hs",
-    "phone" : 3414204880
-   
-    
+    "phone" : 3414204880,
+    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/138847523_2832706463662131_3931400919551961485_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9mcKE7NUWtAAX--kflr&_nc_ht=scontent.fcor10-3.fna&oh=00_AT98Yy1ohfZtqo9xYfvpEV-STiluX_Ipwu2zv9BO4jsi0g&oe=6248A291"
     },
    
    {
@@ -56,8 +53,6 @@ const clinicas = [
     "city": "Mendoza",
     "activeHours": "Lunes a Viernes 9 a 20 hs // Sábado 9 a 13 hs",
     "phone" :  2614234011
-   
-    
     },
    
    {
@@ -66,20 +61,18 @@ const clinicas = [
     "province": "Mendoza" ,
     "city": "Mendoza",
     "activeHours": "Lunes a Viernes 9 a 19 hs // Sábado 9 a 13 hs",
-    "phone" : 2615405474
-   
-    
+    "phone" : 2615405474,
+    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/122866231_2758624857711651_5358198321668575421_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=973b4a&_nc_ohc=a8ZXYgJCT4UAX_UKSVU&_nc_ht=scontent.fcor10-3.fna&oh=00_AT_QiC8YBzLQQTkmuqxH29GuMlaciHFuFhCIoGL2AE_E9g&oe=6249B085"
     },
    
    {
-    "name" : "Clínica Veterinaria Patagonia",
-    "address" : "Juan F. Cobos 231", 
+    "name" : "Veterinaria Mendoza Unimev",
+    "address" : "Bernardo Houssay 612", 
     "province": "Mendoza" ,
-    "city": "Laprida",
-    "activeHours": "Lunes a Viernes 10 a 13 hs // 17 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 2614321309
-   
-    
+    "city": "Mendoza",
+    "activeHours": "Lunes a Sábado 9:30 a 13:30 hs // 17 a 21 hs",
+    "phone" : 02614215216,
+    "image": "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/121625061_3151775641612508_8632100231021941894_n.jpg?stp=cp0_dst-jpg_e15_p403x403_q65&_nc_cat=110&ccb=1-5&_nc_sid=9e2e56&_nc_ohc=mJM-FnJdmrkAX9CyJwB&_nc_ht=scontent.fcor10-3.fna&oh=00_AT9g4GwfMa_X54Ef5xs3yG3y_qxb-9_xn9aBdYyn7nL4Tw&oe=6248E0E4"
     },
    
    {
@@ -88,8 +81,8 @@ const clinicas = [
     "province": "Mendoza" ,
     "city": "Mendoza",
     "activeHours": "Lunes a Viernes 9:30 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 2614233937
-   
+    "phone" : 2614233937,
+    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t31.18172-8/28828308_1973499366299782_3604011053253240126_o.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=FLGx_nE1Z1kAX_muP_b&_nc_ht=scontent.fcor10-3.fna&oh=00_AT_1FrLA1uJBBUwXQAaLfrMrWGNQ6-g-tenpyx_1A_RWHg&oe=62485B03"
     },
    
    {
@@ -98,9 +91,8 @@ const clinicas = [
     "province": "Mendoza" ,
     "city": "Mendoza",
     "activeHours": "24 hs",
-    "phone" : 2614297168
-   
-    
+    "phone" : 2614297168,
+    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.18169-9/312384_211083065701395_954552857_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=de6eea&_nc_ohc=esfAqBTCU8sAX9a8_D1&_nc_ht=scontent.fcor10-3.fna&oh=00_AT-ETHLC-cfk9NYQkx78oeJyswQSEH5LLYGf0l5epBPQCw&oe=6246639F"
     },
    
    {
@@ -109,8 +101,8 @@ const clinicas = [
     "province": "Córdoba",
     "city": "Córdoba",
     "activeHours": "24 hs",
-    "phone" : 3514326414
-    
+    "phone" : 3514326414,
+    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/46505973_328595631057966_4511639080299134976_n.png?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=d8ZMD_WaEHoAX8FsShD&_nc_ht=scontent.fcor10-3.fna&oh=00_AT81xlnItWrnrnC7vvMk5-7JdLA-DoNSsHrvDwEnZV5tvw&oe=624788BD"
     },
    
    {
@@ -119,8 +111,8 @@ const clinicas = [
     "province": "Córdoba",
     "city": "Córdoba",
     "activeHours": "Lunes a Viernes de 9:30 a 12:30 hs // 17 a 20 hs// Sábado 9 a 13 hs",
-    "phone" : 3514842227
-    
+    "phone" : 3514842227,
+    "image" : "https://entornosaludable.com/wp-content/uploads/2019/09/papelmatic-higiene-profesional-limpieza-desinfeccion-clinicas-veterinarias-980x617.jpg"
     },
    
    {
@@ -129,8 +121,8 @@ const clinicas = [
     "province": "Córdoba",
     "city": "Córdoba",
     "activeHours": "Lunes a Viernes de 9:30 js // 17 a 20 hs hs // // Sábado 9 a 13 hs",
-    "phone" : 3514791679
-    
+    "phone" : 3514791679,
+     "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/81845564_142874087151957_6660466584911347712_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=973b4a&_nc_ohc=Vi_w46p9gOgAX_PJq4r&_nc_ht=scontent.fcor10-3.fna&oh=00_AT_1dMQX7eJQIVOuwYJHsMc_ZmjUUR9pNjJZGeg2AQ_Zag&oe=624680C3"
     },
    
    {
@@ -139,10 +131,9 @@ const clinicas = [
     "province": "Córdoba",
     "city": "Córdoba",
     "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 17 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3517631416
-   
-    
-    },
+    "phone" : 3517631416,
+    "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/121257614_1668201766676249_1916816802111887691_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=973b4a&_nc_ohc=DtYnf6JJAAIAX_jkTQz&_nc_ht=scontent.fcor10-3.fna&oh=00_AT_RICEhHswR_qOCDQaTX6gR78mTx_DLy5otxSCe3h38fQ&oe=624762E1"
+   },
    
    {
     "name" : "Bicho's Clínica Veterinaria",
@@ -150,9 +141,8 @@ const clinicas = [
     "province": "Córdoba",
     "city": "Córdoba",
     "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 17 a 20 hs // Sábado 9 a 13 hs",
-    "phone" : 3514658784
-    
-    
+    "phone" : 3514658784,
+     "image" : "https://scontent.fcor10-3.fna.fbcdn.net/v/t1.6435-9/81963394_1269534669913717_5193143501323239424_n.png?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=l7j1BHheP8UAX8Qk0oG&_nc_ht=scontent.fcor10-3.fna&oh=00_AT8ODzj_IQsTyNEeS6N3yZERoHP4VzdMQjj74ZQRLag35A&oe=62491ACE"
     }
    ];
 
