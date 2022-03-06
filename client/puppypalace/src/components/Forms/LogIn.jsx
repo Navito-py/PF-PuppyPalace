3cafee782579711d465eeeb636c27cf08d60e277
Daniel Londoño <danielondonoa@gmail.com>
2022-03-05 23:59:35 -0500

    login styles (#39)
    Co-authored-by: Ivan Toneatto <85807097+Navito-py@users.noreply.github.com>

--- client/puppypalace/src/components/Forms/LogIn.jsx
+++ client/puppypalace/src/components/Forms/LogIn.jsx
@@ -1,42 +1,74 @@
-import React from 'react'
-import { useState } from 'react'
-import {useDispatch} from 'react-redux'
-import { postLogin } from '../../redux/actions'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import { Col, Container, Form, Button, Row } from "react-bootstrap";
+import loginIcon from "../../media/user.png";
+import uiImg from "../../media/uiImg.png";
+import "./Login.css";
 
 export default function LogIn() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const dispatch = useDispatch()
+  function hanleOnChange(e) {
+    e.preventDefault();
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-    const [data,setData] = useState({
-        userName: '',
-        password: ''
-    })
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
-    function hanleOnChange(e){
-        e.preventDefault()
-        setData({
-            ...data,
-            [e.target.name] : e.target.value
-        })
-        console.log(data)
-    }
+  return (
+    <div className="color-back">
+      <Container className="mt-5">
+        <Row>
+          <Col lg={4} md={6} sm={12} className="text-center mt-5 p-5">
+            <img className="icon-img" src={loginIcon} alt="icon" />
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control
+                  onChange={(e) => hanleOnChange(e)}
+                  type="email"
+                  placeholder="Ingrese el email"
+                  name="email"
+                />
+              </Form.Group>
 
-    function handleSubmit(e){
-        e.preventDefault()
-        dispatch(postLogin(data))
-    }
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control
+                  onChange={(e) => hanleOnChange(e)}
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
+              </Form.Group>
 
-    return (
-        <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-            <input onChange={(e) => hanleOnChange(e)} placeholder='Nombre de usuario' type="text" name="userName"/>
-            <input onChange={(e) => hanleOnChange(e)} placeholder='Contraseña' type="password" name="password"/>
-            <button type='submit'>Log in</button>
-            </form>
-            <Link to='/'>
-            <button>Inicio</button>
-            </Link>
-        </div>
-    )
+              <Button
+                onClick={(e) => handleSubmit(e)}
+                variant="primary btn-block"
+                type="submit"
+              >
+                Login
+              </Button>
+
+              <div className="text-left mt-3">
+                <a href="#">
+                  <small className="reset">Olvidaste la Contraseña</small>
+                </a>
+              </div>
+            </Form>
+          </Col>
+
+          <Col lg={8} md={6} sm={12}>
+            <img className="w-100" src={uiImg} alt="icon" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
