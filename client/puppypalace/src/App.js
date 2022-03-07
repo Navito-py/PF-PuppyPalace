54854821be99ef6ec024848b658a3b2bafeedc14
OneTwoDan <danielondonoa@gmail.com>
2022-03-07 12:25:17 -0500

    landing

--- client/puppypalace/src/App.js
+++ client/puppypalace/src/App.js
@@ -7,17 +7,21 @@ import LogIn from './components/Forms/LogIn';
 import Register from './components/Forms/Register';
 import NotFound from "./components/NotFound/NotFound"
 import Detail from './components/Detail/Detail';
+import LandingPage from './components/LandingPage/LandingPage';
+import Emergencies from './components/Emergencies/Emergencies';
 
 export default function App() {
   return (
     <BrowserRouter>
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/formlogin' element={<LogIn />}/>
         <Route path='/formnewuser' element={<Register />}/>
-        <Route path="*" element={<NotFound/>}/>
         <Route path="/clinics/:id" element={<Detail />}/>
+        <Route path="/emergencies" element={<Emergencies />}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </div>
     </BrowserRouter>
---
+++ client/puppypalace/src/components/Emergencies/Emergencies.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Map from "../Map/Map";
+
+export default function Emergencies() {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
