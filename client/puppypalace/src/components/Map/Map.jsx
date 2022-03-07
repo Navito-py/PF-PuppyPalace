affbdff947702f40fbee152268f06c2d4ae573ab
Daniel Londoño <danielondonoa@gmail.com>
2022-03-07 16:19:51 -0500

    mapa version1 (#48)

--- client/puppypalace/src/components/Map/Map.jsx
+++ client/puppypalace/src/components/Map/Map.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import './Map.css'
+import LocationPin from '../LocationPin/LocationPin';
 
-export default function Map() {
+
+export default function Map({ location, zoomLevel }) {
+
   return (
-    <div>
+    <div className="map">
+    <h2 className="map-h2">Mirate ese mapa papá</h2>
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBRQUhLNT4oiXb2CzbNIOlBug5P0RquO9U' }} //api key
+        defaultCenter={location} // es simplemente el centro del mapa cuando se carga por primera vez
+        defaultZoom={zoomLevel} // define la escala inicial del mapa
+      >
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
+        />
+      </GoogleMapReact>
     </div>
+  </div>
   );
 }
+
+
+  
