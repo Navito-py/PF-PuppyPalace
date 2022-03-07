030e512226085ef925d0535c038f996fae8aa396
Luciano Staniszewski <91505393+LucjanStaniszew@users.noreply.github.com>
2022-03-07 15:02:58 -0300

    Object pets (#43)
    * Added 'Gender' to Post Route
    
    * Added 'Gender' property to pet model
    
    * 4 Pets to create

---
+++ back/objectsPets.js
@@ -0,0 +1,47 @@
+const pets = [
+    {
+        "name": "Pía",
+        "gender": "Female",
+        "type" : "Dog",
+        "breed": "Caniche",
+        "age": 10,
+        "height": 15,
+        "weight": 20,
+        "status" : "Lost"
+    },
+
+    {
+        "name": "Amy",
+        "gender": "Female",
+        "type" : "Dog",
+        "breed": "Caniche",
+        "age": 10,
+        "height": 16,
+        "weight": 22,
+        "status" : "Lost"
+    },
+
+    {
+        "name": "Simon",
+        "gender": "Male",
+        "type" : "Cat",
+        "breed": "Callejero",
+        "age": 8,
+        "height": 10,
+        "weight": 12,
+        "status" : "Lost"
+    },
+
+    {
+        "name": "Ella",
+        "gender": "Female",
+        "type" : "Cat",
+        "breed": "Siames",
+        "age": 1,
+        "height": 7,
+        "weight": 10,
+        "status" : "Lost"
+    }
+]
+
+module.exports = { pets }
