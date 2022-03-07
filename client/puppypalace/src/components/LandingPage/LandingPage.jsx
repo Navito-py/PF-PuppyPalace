f56521e1db6457fb96c1336ec62062b68e5e547a
Daniel Londoño <danielondonoa@gmail.com>
2022-03-07 12:59:43 -0500

    landing (#44)

---
+++ client/puppypalace/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./LandingPage.css";
+import { Link } from "react-router-dom";
+
+export default function LandingPage() {
+  return (
+    <div className="landing-back">
+      <h1>Bienvenidos a V.I.Pets</h1>
+
+      <Link to="/formnewuser">
+        <button>Nuevo Usuario</button>
+      </Link>
+
+      <Link to="/formlogin">
+        <button>Login</button>
+      </Link>
+
+      <Link to="/home">
+        <button>Home</button>
+      </Link>
+
+      <Link to="/emergencies">
+        <button>Emergencias</button>
+      </Link>
+    </div>
+  );
+}
---
+++ client/puppypalace/src/components/Map/Map.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+
+const Map = (props) => {
+  return (
+      <GoogleMap 
+        defaultZoom={10}
+        defaultCenter={{ lat: -34.397, lng: 1550.644 }} />
+  )
+};
+
+export default withScriptjs(withGoogleMap(Map));
