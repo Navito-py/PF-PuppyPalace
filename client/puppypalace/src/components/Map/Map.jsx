61cf126adcd86d7ba49c1df8ba8b2981f679dfce
OneTwoDan <danielondonoa@gmail.com>
2022-03-07 13:21:48 -0500

    correccionMap

--- client/puppypalace/src/components/Map/Map.jsx
+++ client/puppypalace/src/components/Map/Map.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
-const Map = (props) => {
+export default function Map() {
   return (
-      <GoogleMap 
-        defaultZoom={10}
-        defaultCenter={{ lat: -34.397, lng: 1550.644 }} />
-  )
-};
-
-export default withScriptjs(withGoogleMap(Map));
+    <div>
+    </div>
+  );
+}
