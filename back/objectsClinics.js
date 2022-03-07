7ee0a7f9446a798b24911a9793c70f531dd20a47
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-07 15:08:36 -0300

    Clinicas nuevas (#46)
    * new clinics and Emergency boolean
    
    * changes on txt

--- back/objectsClinics.js
+++ back/objectsClinics.js
@@ -1,13 +1,45 @@
 const clinicas = [
   {
   "name" : "Clinica Veterinaria Dr. Munini",
-  "address" : "Paraguay 2773, Rosario, Santa Fe", 
+  "address" : "Paraguay 2773", 
   "province": "Santa Fé" ,
   "city": "Rosario",
   "activeHours": "Lunes a Viernes 8 hs a 20 hs // Sábado 9 a 13 hs",
   "phone" : 3414824562,
-  "image" : "https://images.pexels.com/photos/5731866/pexels-photo-5731866.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-   },
+  "image" : "https://images.pexels.com/photos/5731866/pexels-photo-5731866.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 0
+  },
+  {
+  "name" : "URGENVET VETERINARIO 24 HS",
+  "address" : "Cerro Catedral 6099-5901", 
+  "province": "Santa Fé" ,
+  "city": "Rosario",
+  "activeHours": "24 hs",
+  "phone" : 341153718992,
+  "image" : "https://www.gstatic.com/bfe/apps/website/img/h/3348095-dog-cat-1440.jpg",
+  "emergency": 1
+  },
+  {
+    "name" : "Dr. Balbi Emergencias Veterinarias",
+    "address" : "Riccheri 1033", 
+    "province": "Santa Fé" ,
+    "city": "Rosario",
+    "activeHours": "24 hs",
+    "phone" : 341155975108,
+    "image" : "https://www.gstatic.com/bfe/apps/website/img/h/3348095-dog-cat-1440.jpg",
+    "emergency": 1
+    }, 
+ 
+  {
+  "name" : "SiVET",
+  "address" : "Italia 75", 
+  "province": "Santa Fé" ,
+  "city": "Rosario",
+  "activeHours": "24 hs",
+  "phone" : 3414251035,
+  "image" : "https://cdn.pixabay.com/photo/2016/07/19/22/59/veterinary-1529191__340.jpg",
+  "emergency": 1
+  },
  
  {
   "name" : "Mascotas del Oeste",
@@ -16,7 +48,8 @@ const clinicas = [
   "city": "Rosario",
   "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 16 a 20 hs // Sábado 9 a 13 hs",
   "phone" : 3414226932,
-  "image" : "https://images.pexels.com/photos/6235647/pexels-photo-6235647.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/6235647/pexels-photo-6235647.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 0
   },
  {
   "name" : "Veterinaria San Bernardo",
@@ -25,7 +58,8 @@ const clinicas = [
   "city": "Rosario",
   "activeHours": "Lunes a Viernes 9:30 a 13 hs // 17 a 19 hs // Sábado 9 a 13 hs",
   "phone" : 3412419480,
-  "image" : "https://images.pexels.com/photos/5733422/pexels-photo-5733422.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/5733422/pexels-photo-5733422.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 0
   },
  {
   "name" : "Asistencia Veterinaria",
@@ -34,7 +68,8 @@ const clinicas = [
   "city": "Rosario",
   "activeHours": "Lunes a Viernes 9 a 12 hs // 17 a 20 hs // Sábado 9 a 13 hs",
   "phone" : 3413076530,
-  "image" : "https://images.pexels.com/photos/7474560/pexels-photo-7474560.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/7474560/pexels-photo-7474560.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 0
   },
  
  {
@@ -44,19 +79,21 @@ const clinicas = [
   "city": "Rosario",
   "activeHours": "24 hs",
   "phone" : 3414204880,
-  "image" : "https://images.pexels.com/photos/6234980/pexels-photo-6234980.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/6234980/pexels-photo-6234980.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 1
   },
  
- {
-  "name" : "Clínica Veterinaria Francesa",
-  "address" : "Buenos Aires 373", 
-  "province": "Mendoza",
-  "city": "Mendoza",
-  "activeHours": "Lunes a Viernes 9 a 20 hs // Sábado 9 a 13 hs",
-  "phone" :  2614234011,
-  "image" : "https://media.istockphoto.com/photos/young-happy-veterinary-nurse-smiling-while-playing-with-a-dog-high-picture-id1303362255?b=1&k=20&m=1303362255&s=170667a&w=0&h=ebm7UoUmaCTxepZ9NoyPRTK2WC-5TmTlk6PyXou_NFg="
-  },
- 
+{
+"name" : "Clínica Veterinaria Francesa",
+"address" : "Buenos Aires 373", 
+"province": "Mendoza",
+"city": "Mendoza",
+"activeHours": "Lunes a Viernes 9 a 20 hs // Sábado 9 a 13 hs",
+"phone" :  2614234011,
+"image" : "https://media.istockphoto.com/photos/young-happy-veterinary-nurse-smiling-while-playing-with-a-dog-high-picture-id1303362255?b=1&k=20&m=1303362255&s=170667a&w=0&h=ebm7UoUmaCTxepZ9NoyPRTK2WC-5TmTlk6PyXou_NFg=",
+"emergency": 0
+},
+
  {
   "name" : "Vet House Clínica Veterinaria",
   "address" : "Juan Gualberto Godoy 181", 
@@ -64,7 +101,8 @@ const clinicas = [
   "city": "Mendoza",
   "activeHours": "Lunes a Viernes 9 a 19 hs // Sábado 9 a 13 hs",
   "phone" : 2615405474,
-  "image" : "https://media.istockphoto.com/photos/image-of-male-doctor-veterinarian-with-stethoscope-is-holding-cute-picture-id1294792706?b=1&k=20&m=1294792706&s=170667a&w=0&h=hq8H-P-omwVecfNPnuECsfXTo33rfKajIK1RRJfP0xs="
+  "image" : "https://media.istockphoto.com/photos/image-of-male-doctor-veterinarian-with-stethoscope-is-holding-cute-picture-id1294792706?b=1&k=20&m=1294792706&s=170667a&w=0&h=hq8H-P-omwVecfNPnuECsfXTo33rfKajIK1RRJfP0xs=",
+  "emergency": 0
   },
  
  {
@@ -74,7 +112,8 @@ const clinicas = [
   "city": "Mendoza",
   "activeHours": "Lunes a Sábado 9:30 a 13:30 hs // 17 a 21 hs",
   "phone" : 02614215216,
-  "image": "https://media.istockphoto.com/photos/vet-doctor-examining-labrador-dog-picture-id1303833951?b=1&k=20&m=1303833951&s=170667a&w=0&h=kZOqKof2-6XnBpkFzRqcJTitSu9_4uxmcYXiJp4aXMk="
+  "image": "https://media.istockphoto.com/photos/vet-doctor-examining-labrador-dog-picture-id1303833951?b=1&k=20&m=1303833951&s=170667a&w=0&h=kZOqKof2-6XnBpkFzRqcJTitSu9_4uxmcYXiJp4aXMk=",
+  "emergency": 0
   },
  
  {
@@ -84,9 +123,31 @@ const clinicas = [
   "city": "Mendoza",
   "activeHours": "Lunes a Viernes 9:30 a 20 hs // Sábado 9 a 13 hs",
   "phone" : 2614233937,
-  "image" : "https://images.pexels.com/photos/4917820/pexels-photo-4917820.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/4917820/pexels-photo-4917820.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 0
   },
  
+{
+"name" : "Arrayanes Centro Veterinario 24hs",
+"address" : "Paraná 55", 
+"province": "Mendoza" ,
+"city": "Mendoza",
+"activeHours": "24 hs",
+"phone" : 2614246810,
+"image" : "https://img.freepik.com/foto-gratis/algunas-vitaminas-cachorros-pequenos_329181-14493.jpg?size=626&ext=jpg",
+"emergency": 1
+},
+
+{
+  "name" : "HOSPITAL VETERINARIO ANDINO",
+  "address" : "Los Carolinos Manzana 16 Casa 7", 
+  "province": "Mendoza" ,
+  "city": "Mendoza",
+  "activeHours": "24 hs",
+  "phone" : 2614444396,
+  "image" : "https://img.freepik.com/foto-gratis/doctor-llevando-perrito-gris_329181-10394.jpg?size=626&ext=jpg&uid=R60651945",
+  "emergency": 1
+  }, 
  {
   "name" : "Veterinaria Dr. de la Torre",
   "address" : "Jorge A. Calle 224", 
@@ -94,7 +155,8 @@ const clinicas = [
   "city": "Mendoza",
   "activeHours": "24 hs",
   "phone" : 2614297168,
-  "image" : "https://images.pexels.com/photos/4580783/pexels-photo-4580783.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/4580783/pexels-photo-4580783.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 1
   },
  
  {
@@ -104,7 +166,8 @@ const clinicas = [
   "city": "Córdoba",
   "activeHours": "24 hs",
   "phone" : 3514326414,
-  "image" : "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 1
   },
  
  {
@@ -114,7 +177,8 @@ const clinicas = [
   "city": "Córdoba",
   "activeHours": "Lunes a Viernes de 9:30 a 12:30 hs // 17 a 20 hs// Sábado 9 a 13 hs",
   "phone" : 3514842227,
-  "image" : "https://images.pexels.com/photos/320014/pexels-photo-320014.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/320014/pexels-photo-320014.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 0
   },
  
  {
@@ -124,7 +188,8 @@ const clinicas = [
   "city": "Córdoba",
   "activeHours": "Lunes a Viernes de 9:30 js // 17 a 20 hs hs // // Sábado 9 a 13 hs",
   "phone" : 3514791679,
-   "image" : "https://images.pexels.com/photos/1741205/pexels-photo-1741205.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+   "image" : "https://images.pexels.com/photos/1741205/pexels-photo-1741205.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+   "emergency": 0
   },
  
  {
@@ -134,7 +199,8 @@ const clinicas = [
   "city": "Córdoba",
   "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 17 a 20 hs // Sábado 9 a 13 hs",
   "phone" : 3517631416,
-  "image" : "https://images.pexels.com/photos/825947/pexels-photo-825947.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  "image" : "https://images.pexels.com/photos/825947/pexels-photo-825947.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "emergency": 0
  },
  
  {
@@ -144,8 +210,31 @@ const clinicas = [
   "city": "Córdoba",
   "activeHours": "Lunes a Viernes 9:30 a 12:30 hs // 17 a 20 hs // Sábado 9 a 13 hs",
   "phone" : 3514658784,
-   "image" : "https://images.pexels.com/photos/1612861/pexels-photo-1612861.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-  }
+   "image" : "https://images.pexels.com/photos/1612861/pexels-photo-1612861.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+   "emergency": 0
+  },
+
+  {
+    "name" : "Veterinaria EIKOS Urgencias 24HS",
+    "address" : "Bv. Los Granaderos 3170", 
+    "province": "Córdoba",
+    "city": "Córdoba",
+    "activeHours": "24 hs",
+    "phone" :  3513651916,
+    "image" : "https://cdn.pixabay.com/photo/2021/02/01/00/52/dog-5969004__340.jpg",
+    "emergency": 1
+    },
+     
+    {
+    "name" : "Hospital Veterinario Córdoba",
+    "address" : "Fructuoso Rivera 35", 
+    "province": "Córdoba",
+    "city": "Córdoba",
+    "activeHours": "24 hs",
+    "phone" :  3514282426,
+    "image" : "https://cdn.pixabay.com/photo/2019/05/07/12/53/vet-4185908__340.jpg",
+    "emergency": 1
+    }
  ];
  
    module.exports = {clinicas};
