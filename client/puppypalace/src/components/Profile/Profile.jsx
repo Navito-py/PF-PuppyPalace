ae88a42a310ef1d7d7338fb07ccd487348630e70
ivan ariel toneatto <ivan-ariel@hotmail.com>
2022-03-08 18:51:34 -0300

    formulario de creacion de mascotas

--- client/puppypalace/src/components/Profile/Profile.jsx
+++ client/puppypalace/src/components/Profile/Profile.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Profile() {
     return (
         <div>
-            Profile
+            <img src='https://pm1.narvii.com/6650/1e1210b9c20ad495d949496e77b195597813315f_hq.jpg' width = '100px' height='100px'/>
+            <p>Nombre de Usurario: </p>
+            <p>Nombres: </p>
+            <p>Direccion: </p>
+            <Link to ='createPet'>
+            <button >Crear mascota</button>
+            </Link>
         </div>
     )
 }
