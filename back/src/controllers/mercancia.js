3d85e630a5c26e40dbb6aa40fead9ca18efce6e7
lautaro321 <skoollarris9@gmail.com>
2022-03-12 14:54:27 -0300

    mercadopago teriminado

--- back/src/controllers/mercancia.js
+++ back/src/controllers/mercancia.js
@@ -42,3 +42,40 @@ const mercadopago = require("mercadopago")
 }
 
 module.exports = {prueba, notificacionprueba};
+
+
+//usarios de prueba 
+
+/*{
+    "id": 1088684070,
+    "nickname": "TETE5137588",
+    "password": "qatest8178",
+    "site_status": "active",
+    "email": "test_user_61848221@testuser.com"
+}
+
+{
+    "id": 1088678026,
+    "nickname": "TETE5089007",
+    "password": "qatest9216",
+    "site_status": "active",
+    "email": "test_user_53541193@testuser.com"
+}
+
+Tarjetas de prueba
+Tarjeta	Número	Código de seguridad	Fecha de vencimiento
+Mastercard	5031 7557 3453 0604	123	11/25
+Visa	4509 9535 6623 3704	123	11/25
+American Express	3711 803032 57522	1234	11/25
+
+Para probar distintos resultados de pago, completa el estado deseado en el nombre del titular de la tarjeta:
+Estado de pago	Descripción
+APRO	Pago aprobado
+CONT	Pago pendiente
+OTHE	Rechazado por error general
+CALL	Rechazado con validación para autorizar
+FUND	Rechazado por monto insuficiente
+SECU	Rechazado por código de seguridad inválido
+EXPI	Rechazado por problema con la fecha de expiración
+FORM	Rechazado por error en formulario
+*/
