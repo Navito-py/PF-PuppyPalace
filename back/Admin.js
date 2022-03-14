80e700f71e29630bbb69e529b219c0420ff88a69
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-14 17:39:08 -0300

    dale dale

---
+++ back/Admin.js
@@ -0,0 +1,123 @@
+const users = [
+{
+ "userName":"Vipets",
+ "name":"Vip",
+ "lastName":"pets",
+ "isAdmin": true,   
+ "email":"vipets@gmail.com",
+ "password":"soyunprinceso",
+ "phone":"1122445577",
+ "adress":"Rio Santa Rosa 1000",
+ "province":"Cordoba",
+ "city":"Cordoba",
+ "image":"../client/puppypalace/src/media/logoVIPets2.png"
+},
+{
+"userName":"ValeAla",
+"name":"Valeria",
+"lastName":"Alasia",
+"isAdmin": false,   
+"email":"ValeAla@gmail.com",
+"password":"123456789",
+"phone":"1122445578",
+"adress":"Av. San Martin 611",
+"province":"Santa Fe",
+"city":"Rosario",
+"image":"https://i.pinimg.com/736x/d5/74/32/d574325bbf7afd4515bf774dd8aa965d.jpg"
+},
+{
+"userName":"Navito",
+"name":"Ivan",
+"lastName":"Toneatto",
+"isAdmin": false,   
+"email":"ivanariel@hotmail.com",
+"password":"holaloco20",
+"phone":"1234567899",
+"adress":"Calle 46 450",
+"province":"Cordoba",
+"city":"Cordoba",
+"image":"https://img2.rtve.es/imagenes/yes-chad-uno-memes-mas-famosos-internet/1633090530832.jpg"
+}, 
+{
+"userName":"Nacho",
+"name":"Hernan",
+"lastName":"Cabrera Zabala",
+"isAdmin": false,   
+"email":"Nacho@hotmail.com",
+"password":"sereparacalzados",
+"phone":"3451552233",
+"adress":"Mitre 350",
+"province":"Cordoba",
+"city":"Cordoba",
+"image":"https://static.wikia.nocookie.net/lossimpson/images/f/ff/Lionel_hutz_abogado.jpg/revision/latest?cb=20121013172137&path-prefix=es"
+},
+{
+"userName":"Lauti",
+"name":"Lautaro",
+"lastName":"Martin",
+"isAdmin": false,   
+"email":"martinsinacento@gmail.com",
+"password":"amimir",
+"phone":"1122336699",
+"adress":"Av. del Mirador 500",
+"province":"Mendoza",
+"city":"Mendoza",
+"image":"https://i.pinimg.com/originals/4f/24/14/4f2414175316ada65c072bfd430abc94.jpg"
+},
+{
+"userName":"marian",
+"name":"Mariano",
+"lastName":"Farrate",
+"isAdmin": false,   
+"email":"mfarrate@gmail.com",
+"password":"skollskoll",
+"phone":"7788996655",
+"adress":"Mitre 650",
+"province":"Cordoba",
+"city":"Cordoba",
+"image":"https://static1-es.millenium.gg/articles/4/35/12/4/@/163249-9ebnvctq5sv31-article_image_t-1.jpg"
+},
+{
+"userName":"yuyu",
+"name":"Luciano",
+"lastName":"Staniszewski",
+"isAdmin": false,   
+"email":"lucjan@gmail.com",
+"password":"yuyu1997",
+"phone":"1133228899",
+"adress":"Rio Santa Rosa 715",
+"province":"Cordoba",
+"city":"Cordoba",
+"image":"https://s1.eestatic.com/2021/02/04/actualidad/556457139_172092925_854x640.jpg"
+},
+{
+"userName":"SilviStani",
+"name":"Silvina",
+"lastName":"Staniszewski",
+"isAdmin": false,   
+"email":"silvista@gmail.com",
+"password":"asiqnotegustamicss",
+"phone":"1163632288",
+"adress":"Rio Santa Rosa 715",
+"province":"Cordoba",
+"city":"Cordoba",
+"image":"https://www.sportfem.es/wp-content/uploads/2020/01/Sin-ti%CC%81tulo-1-1-560x416.png"
+},
+{
+"userName":"DaniLondonio",
+"name":"Daniel",
+"lastName":"Londonio",
+"isAdmin": false,   
+"email":"danilondonio@gmail.com",
+"password":"familiasiempre",
+"phone":"543215987",
+"adress":"En su casa 700",
+"province":"Santa Fe",
+"city":"Rosario",
+"image":"https://static.wikia.nocookie.net/wowpedia/images/d/d5/BTNSamuro.png/revision/latest/smart/width/250/height/250?cb=20210516025049"
+},
+]
+
+module.exports = {
+    users
+} 
