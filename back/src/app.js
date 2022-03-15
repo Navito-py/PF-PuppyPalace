c07e8e769ed93469b943d06083b6621f854d6ded
nachocabreraz <hcabrerazavala@gmail.com>
2022-03-15 19:49:58 -0300

    dale

--- back/src/app.js
+++ back/src/app.js
@@ -18,6 +18,8 @@ server.name = 'API';
 //   })
 // )
 
+
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
