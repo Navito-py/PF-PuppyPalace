0f05f2c99b25c67ebb1e83cb316a86bb1c09872a
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-03-17 17:44:45 -0300

    Reserve Model modified

--- back/src/models/Reserve.js
+++ back/src/models/Reserve.js
@@ -17,7 +17,7 @@ module.exports = (sequelize) => {
         },
 
         date: {
-            type: DataTypes.DATE,
+            type: DataTypes.DATEONLY,
             allowNull: false,
             validate:{
                 isAfter: DataTypes.NOW
