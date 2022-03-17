bad5be7b0fd62bcba1cd36553e82b652f881d976
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-17 11:39:53 -0300

    model

--- back/src/models/clinic.js
+++ back/src/models/clinic.js
@@ -76,7 +76,17 @@ module.exports = (sequelize) => {
         hospitalization : {
             type: DataTypes.BOOLEAN,
            // allowNull: false,
-        }
+        },
+
+        reserveDay : {
+            type: DataTypes.ENUM("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"),
+           // allowNull: false,          
+        },
+
+        reserveTime : {
+            type: DataTypes.ENUM("8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"),
+           // allowNull: false,          
+        },
 
     }, {timestamps: false})
 };
