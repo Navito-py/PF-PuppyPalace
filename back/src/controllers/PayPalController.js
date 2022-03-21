5ddafe71c2407fc09630c4ee2502a712052f4b1b
Valeria Alasia <87764505+Memuse90@users.noreply.github.com>
2022-03-21 19:08:46 -0300

    Paypaaaaaallll (#107)
    * Quitar Auth de ruta pets
    
    * Cambiamos a PAyPAl y ANDA!

---
+++ back/src/controllers/PayPalController.js
@@ -0,0 +1,61 @@
+const request = require ('request');
+
+const auth ={
+     user: process.env.PAYPAL_CLIENT, 
+     pass: process.env.PAYPAL_SECRET 
+
+}
+let paymentData = {};
+const createPayment = (req, res) => {
+
+    const body = {
+        intent: 'CAPTURE',
+        purchase_units: [{
+            amount: {
+                currency_code: 'USD', //https://developer.paypal.com/docs/api/reference/currency-codes/
+                value: '10'
+            }
+        }],
+        application_context: {
+            brand_name: `Vipets`,
+            landing_page: 'NO_PREFERENCE', // Default, para mas informacion https://developer.paypal.com/docs/api/orders/v2/#definition-order_application_context
+            user_action: 'PAY_NOW', // Accion para que en paypal muestre el monto del pago
+            return_url: `http://localhost:3001/payment/execute-payment`, // Url despues de realizar el pago
+            cancel_url: `http://localhost:3001/payment/cancel-payment` // Url despues de realizar el pago
+        }
+    }
+    //https://api-m.sandbox.paypal.com/v2/checkout/orders [POST]
+
+    request.post(`${process.env.PAYPAL_API}/v2/checkout/orders`, {
+        auth,
+        body,
+        json: true
+    }, (err, response) => {
+        console.log(err);
+        res.json({ data: response.body.links[1].href })
+    })
+    
+}
+const executePayment = (req, res) => {
+    const token = req.query.token; //<-----------
+
+    request.post(`${process.env.PAYPAL_API}/v2/checkout/orders/${token}/capture`, {
+        auth,
+        body: {},
+        json: true
+    }, (err, response) => {
+        paymentData = response.body;
+        if(response.body.status === 'COMPLETED'){
+            res.redirect('https://vipets.vercel.app/home/profile')
+        } else {
+            res.send('error');
+        }
+        
+    })
+};
+
+
+module.exports = {
+    createPayment,
+    executePayment
+}
