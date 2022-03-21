78c075c65f10ee81de6c1d553bebebd18ea17f37
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-03-21 20:01:20 -0300

    Updated Visit model

--- back/src/models/Visit.js
+++ back/src/models/Visit.js
@@ -2,25 +2,27 @@ const {DataTypes} = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define ('visit', {
-      
-        date: {
-            type: DataTypes.DATEONLY,
-            allowNull: false,
-        },
 
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: true,
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            unique: true,
+            primaryKey: true
         },
+      
+        // date: {
+        //     type: DataTypes.DATEONLY,
+        //     allowNull: false,
+        // },
 
         review : {
             type: DataTypes.TEXT,
-            
         },
 
         score: {
             type: DataTypes.ENUM("1", "2", "3", "4", "5"),
-        },
+        }
 
-       
-    })}
+    }, {timestamps: false})
+}
