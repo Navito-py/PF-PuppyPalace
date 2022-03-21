cb54fa78e6021cc02b518925164949d981b31c94
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-03-21 20:31:56 -0300

    route for getting reviews

--- back/src/routes/Clinics.js
+++ back/src/routes/Clinics.js
@@ -1,9 +1,11 @@
 const { Router } = require ('express');
 const { getClinics, getClinicsId } = require("../controllers/Clinics.js");
+const { getClinicsReview } = require('../controllers/Review.js');
 
 const router = Router();
 
 router.get('/', getClinics);
 router.get('/:id', getClinicsId);
+router.get('/review/:id', getClinicsReview);
 
 module.exports = router;
