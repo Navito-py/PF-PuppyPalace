55a2f210a0e7e55e852d351c273f2185948fd3ba
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-03-21 20:21:06 -0300

    Cleaned Code

--- back/src/routes/Clinics.js
+++ back/src/routes/Clinics.js
@@ -1,10 +1,9 @@
 const { Router } = require ('express');
-const { getClinics, getClinicsId, postReserve, getReserve } = require("../controllers/Clinics.js");
+const { getClinics, getClinicsId } = require("../controllers/Clinics.js");
 
 const router = Router();
 
 router.get('/', getClinics);
 router.get('/:id', getClinicsId);
-router.post('/reserve', postReserve);
 
 module.exports = router;
