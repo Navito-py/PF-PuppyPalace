c09141241615b2c6cdd6e1083c33078641f6fed5
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-20 22:31:00 -0300

    emergency css ready

--- client/puppypalace/src/components/Emergencies/Emergencies.jsx
+++ client/puppypalace/src/components/Emergencies/Emergencies.jsx
@@ -6,11 +6,14 @@ import {Link} from 'react-router-dom'
 export default function Emergencies() {
   return (
     <div className="emergencies">
+      <div className="TopBar-Emergencies">
       <Link to='/'>
         <button className="buttonHome"><img src="https://cdn-icons-png.flaticon.com/512/5100/5100262.png" alt="Home" height="50px"/></button>
-        </Link>
+      </Link>
+      </div>
       <h1 className="title-emergencies">Emergencias</h1>
       <Map />
+      <div className="break-emergency"><img className="center-img" src="https://cdn-icons-png.flaticon.com/512/1581/1581645.png" alt="" height="50px"/> VIPets <img className="centerimg" src="https://cdn-icons-png.flaticon.com/512/1581/1581645.png" alt="" height="50px"/></div>
     </div>
   );
 }
