df4ec83105f4b1401d4abc3cacec846ca1616541
Ivan Toneatto <85807097+Navito-py@users.noreply.github.com>
2022-03-22 18:43:40 -0300

    Filterusers (#116)
    * searchbar de users y clinicas, put de clinicas
    
    * terminado el put de clinicas

--- client/puppypalace/src/components/AdminForms/ModifyClinic.jsx
+++ client/puppypalace/src/components/AdminForms/ModifyClinic.jsx
@@ -2,28 +2,34 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {getDetail} from '../../redux/actions'
+
+import {getDetail, modifyClinic} from '../../redux/actions'
+
 
 export default function ModifyClinic() {
     const dispatch = useDispatch()
     const { id } = useParams()
-    const clinicmod = useSelector((state) => state.detail)
+    const authToken = sessionStorage.getItem('token')
+    let clinicmod = useSelector((state) => state.detail)
 
     useEffect(() => {
         dispatch(getDetail(id))
-        console.log(data)
-    },[dispatch, id])
+    },[dispatch])
 
-    const [data, setData] = useState(clinicmod)
 
     function handleOnChange(e){
         e.preventDefault()
-        const { name, value } = e.target;
-        setData({
-            ...data,
-            [name] : value
-        })
-        console.log(data)
+        clinicmod = {
+            ...clinicmod,
+            [e.target.name] : e.target.value
+        }
+        console.log(clinicmod)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        dispatch(modifyClinic(id, clinicmod, authToken))
+
     }
 
     return (
@@ -33,27 +39,15 @@ export default function ModifyClinic() {
             </Link>
             {
                 clinicmod.name?
-            <form>
+            <form onSubmit={e => handleSubmit(e)}>
                 <input type='text' onChange={e => handleOnChange(e)} placeholder={clinicmod.name} name='name'/>
                 <input type='text' onChange={e => handleOnChange(e)} placeholder={clinicmod.address} name='address'/>
-                <select>
-                    <option hidden>Provincia</option>
-                    <option value='Córdoba'>Córdoba</option>
-                    <option value='Mendoza'>Mendoza</option>
-                    <option value='Santa Fe'>Santa Fe</option>
-                </select>
-                <select>
-                    <option hidden>Cuidad</option>
-                    <option value='Córdoba'>Córdoba</option>
-                    <option value='Mendoza'>Mendoza</option>
-                    <option value='Rosario'>Rosario</option>
-                </select>
-                <label style={{margin:'10px'}}>{clinicmod.province}</label>
-                <label>{clinicmod.city}</label>
                 <input onChange={e => handleOnChange(e)} type='text' placeholder={clinicmod.activeHours} name='activeHours'/>
-                <input onChange={e => handleOnChange(e)} type='text' placeholder={clinicmod.email? clinicmod.email : 'No encontrado'} name='email'/>
+                <input onChange={e => handleOnChange(e)} type='text' placeholder={clinicmod.email? clinicmod.email : 'Email no encontrado'} name='email'/>
                 <input onChange={e => handleOnChange(e)} type='text' placeholder={clinicmod.phone} name='phone'/>
                 <input onChange={e => handleOnChange(e)} type='text' placeholder={clinicmod.image} name='image'/>
+                <input onChange={e => handleOnChange(e)} type='text' placeholder={clinicmod.emergency} name='emergency'/>
+                <input onChange={e => handleOnChange(e)} type='text' placeholder={clinicmod.hospitalization} name='hospitalization'/>
                 <button type='submit'>Modificar</button>
             </form> : <p>loading...</p>
             }
--- client/puppypalace/src/redux/reducer/index.js
+++ client/puppypalace/src/redux/reducer/index.js
@@ -120,6 +120,15 @@ function rootReducer(state = initialState, action) {
         ...state,
         clinics: clinicfilted
       }
+
+    case 'FILTER_ADMIN_CLINICS':
+      const allClinicsFilter = state.allClinics
+      const clinicfilted = action.payload === ''? allClinicsFilter : 
+      allClinicsFilter.filter(e => e.name.toLowerCase().includes(action.payload.toLowerCase()))
+      return{
+        ...state,
+        clinics: clinicfilted
+      }
     case 'GET_PAYMENT_REDIR':
       console.log(action.payload)
       return {
