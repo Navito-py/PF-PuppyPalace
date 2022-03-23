78ce21d57e5c219b2856258807ec3882e3d1f868
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-23 18:35:44 -0300

    at Last!!!!

--- client/puppypalace/src/components/Card/ReserveCard.jsx
+++ client/puppypalace/src/components/Card/ReserveCard.jsx
@@ -9,15 +9,15 @@ import {
   MDBBtn
 } from "mdb-react-ui-kit";
 
-export default function ReserveCard({ clinicName, date, hourly, clinicPhone, ammount, description }) {
+export default function ReserveCard({ clinicName, date, hourly, clinicPhone, description }) {
   return (
     <div >
       <MDBCard className="card-reserve">
         <MDBCardBody className="body-reserve">
+          <MDBCardTitle className="nameT">Datos de la reserva:</MDBCardTitle>
           <MDBCardTitle className="name"> {clinicName}</MDBCardTitle>
-          <MDBCardText>{date}</MDBCardText>
-          <MDBCardText>{hourly}</MDBCardText>
-          <MDBCardText>{ammount}</MDBCardText>
+          <MDBCardText>Fecha: {date}</MDBCardText>
+          <MDBCardText>Hora: {hourly}</MDBCardText>
           <MDBCardText>{description}</MDBCardText>
           <MDBCardText><img src="https://cdn-icons-png.flaticon.com/512/7129/7129257.png" alt="" height="30px" />{clinicPhone}</MDBCardText>
         </MDBCardBody>
