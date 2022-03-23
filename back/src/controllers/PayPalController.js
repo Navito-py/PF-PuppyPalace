a8b72adbc52fd114b0def758b2a0f1210dd5bae8
Memuse90 <87764505+Memuse90@users.noreply.github.com>
2022-03-23 19:06:07 -0300

    return url

--- back/src/controllers/PayPalController.js
+++ back/src/controllers/PayPalController.js
@@ -20,7 +20,7 @@ const createPayment = (req, res) => {
             brand_name: `Vipets`,
             landing_page: 'NO_PREFERENCE', // Default, para mas informacion https://developer.paypal.com/docs/api/orders/v2/#definition-order_application_context
             user_action: 'PAY_NOW', // Accion para que en paypal muestre el monto del pago
-            return_url: `http://localhost:3001/payment/execute-payment`, // Url despues de realizar el pago
+            return_url: `https://vipets.herokuapp.com/payment/execute-payment`, // Url despues de realizar el pago
             cancel_url: `http://localhost:3001/payment/cancel-payment` // Url despues de realizar el pago
         }
     }
