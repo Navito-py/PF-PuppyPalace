5df5338c1b158d4711c7ad4ab885aea51fa8961d
Silvina Staniszewski <silvina.stani@gmail.com>
2022-03-26 15:04:41 -0300

    pruebaVistas

--- client/puppypalace/src/components/Card/Card.jsx
+++ client/puppypalace/src/components/Card/Card.jsx
@@ -13,7 +13,9 @@ export default function Card({ name, image, schedule, province, emergency, phone
   return (
     <div className="card-clinic">
       <MDBCard className="size-card">
+        <div className="card-front">
         <MDBCardImage src={image} className='image-card' />
+        </div>
         <MDBCardBody className="body-card">
           <MDBCardTitle className="card-title">{name}</MDBCardTitle>
           <MDBCardText >{province}</MDBCardText>
